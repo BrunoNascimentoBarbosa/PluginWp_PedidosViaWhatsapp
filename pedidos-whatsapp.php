fc978d7ea985984889d421edb70459f46389c6fb
Bruno Nascimento <bruno@brunonascimento.com>
2022-02-06 18:20:19 -0300

    Melhorias no formulário e configuracao do envio para o whatsapp

--- pedidos-whatsapp.php
+++ pedidos-whatsapp.php
@@ -9,23 +9,26 @@
 */
 
 #Função para enviar o pedido via whatsapp.
+
+ 
 function pedido(){ 
-    $pedido = $_POST['pedido'];
-    $local = $_POST['local'];
-    $orcamento = $_POST['valor'];
-    $nome = $_POST['nome'];
-    $telefone = $_POST['telefone'];
-
-
+   $produto = $_POST['produto'];
+   $obs = $_POST['pedido'];
+   $local = $_POST['local'];
+   $nome = $_POST['nome'];
+   $telefone = $_POST['telefone'];
     
-    echo "Tela de pedidos";
-     
-    $texto_whatsapp = "Olá meu nome é *" . $nome . "*, desejo fazer um pedido:"."\n " .$pedido . "o local de entre é: *". $local. "* *Tentativas*: "." *O prestado *:".".Tenho um orçamento de:*".$orcamento. "* meu telefone é: *".$telefone."*" ;
-
-    $mgs_whats = str_replace (' ','%20',$texto_whatsapp);
-
-    $action_url = "https://api.whatsapp.com/send?phone=5521974561726&text=$mgs_whats";
    
+        
+   echo "<h3>Faça o seu pedidos via whatsapp é simple e rápido.</h3>";
+     
+   $texto_whatsapp = "Olá meu nome é " .$nome. " .Quero fazer um pedido do *produto*: " .$produto. ". Entrega é no endereço: " .$local. "  *Observação*: " .$obs ;
+
+   $mgs_whats = str_replace (' ','%20',$texto_whatsapp);
+ 
+
+   $action_url = "https://api.whatsapp.com/send?phone=5521974561726&text=$mgs_whats";
+                    
 
     ?>
  
@@ -33,7 +36,7 @@ function pedido(){
     <?php if(isset($_POST['submit'])){?>
 
     <script type="text/javascript">
-    window.location = "<?php echo $action_url;?>";
+    window.location = "<?php echo $action_url; ?>";
     </script>
     <?php }  ?>
     
@@ -41,33 +44,32 @@ function pedido(){
 
     <form action="" method="post">
 
-		
-		<label for="story">Detalhe o seu pedido</label>
+		<label for="">Qual produto você deseja? </label>
+    <select name="produto" id="">
+      <option value="Produto 1">Produto 1 </option>
+      <option value="Produto 2">Produto 2 </option>
+      <option value="Produto 3">Produto 3 </option>
+      <option value="Produto 4">Produto 4 </option>
+    </select>
+		<label for="story">Alguma observação no pedido?</label>
 
-       <textarea id="story" name="pedido"
-          rows="5" cols="33"> Ex: Quero uma pizza.
-       </textarea>
+       <input type="text"id="story" name="pedido"
+          rows="4" cols="50" placeholder="Ex: Preciso da entrega com urgência." >  
+    </input>
        <br>
 
 
         <label for="">Qual o local de entrega do seu pedido? </label>
-        <input type="text" name="local">
+        <input type="text" name="local" placeholder="Rua Dr Andre Barbosa N 35">
         <br>
              
-        
-
-         
-        <br>
-
-        <label for="">Qual o orçamento disponível para o pedido? (R$)  </label>
-        <input type="number" name="valor" id="">
-        <br>
+      
         
         <label for="" > Qual o seu nome ? </label>
         <input type="text" name="nome">  
         <br>
 
-        <label> Através de qual whatsapp podemos entrar em contato?</label>
+        <label> Qual o seu whatsapp?</label>
         <input type="tel" name="telefone" size="40" placeholder="(00)00000-0000" data-mask="(__)_____-____">
         <br>
         <input type="submit" name="submit" value="Enviar o pedido">
@@ -75,6 +77,8 @@ function pedido(){
     </form>
 
   <?php 
-
+ 
 }
+
 add_shortcode('solicitar_pedido','pedido'); 
+ 
